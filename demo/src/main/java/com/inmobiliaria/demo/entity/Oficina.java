bb612fd043ed57344bf8a3da7959dc642ca7e46c
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-04-11 18:52:39 -0500

    cambios

--- demo/src/main/java/com/inmobiliaria/demo/entity/Oficina.java
+++ demo/src/main/java/com/inmobiliaria/demo/entity/Oficina.java
@@ -1,5 +1,23 @@
 package com.inmobiliaria.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="oficina")
 public class Oficina {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
     
+    private String zona;
 }
