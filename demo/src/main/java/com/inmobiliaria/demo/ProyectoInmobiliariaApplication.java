083fb95063113782f81b89391173fb4d90e85117
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-04-11 14:46:40 -0500

    nuevos

---
+++ demo/src/main/java/com/inmobiliaria/demo/ProyectoInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/inmobiliaria/demo/ProyectoInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobiliaria.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
