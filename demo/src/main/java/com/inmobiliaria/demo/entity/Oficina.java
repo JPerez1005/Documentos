7c2c6fe9b20cb27b4de80e042fa87dc2746a5532
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-04-11 18:47:26 -0500

    sobreescribiendo

--- demo/src/main/java/com/inmobiliaria/demo/entity/Oficina.java
+++ demo/src/main/java/com/inmobiliaria/demo/entity/Oficina.java
@@ -1,35 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.inmobiliaria.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
- *
- * @author camper
- */
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name="oficina")
 public class Oficina {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
     
-    private String zona;
-    
-    @OneToOne(mappedBy = "oficina")
-    private Inmueble inmueble;
 }
